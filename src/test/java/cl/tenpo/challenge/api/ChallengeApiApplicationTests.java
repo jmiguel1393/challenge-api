e7bbae9ae4234db8a1af665ccc917f25f49e94cb
Jose Miguel <jmnr.1311993@gmail.com>
2023-01-31 13:08:45 -0500

    build: initializing repository

---
+++ src/test/java/cl/tenpo/challenge/api/ChallengeApiApplicationTests.java
@@ -0,0 +1,11 @@
+package cl.tenpo.challenge.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChallengeApiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
