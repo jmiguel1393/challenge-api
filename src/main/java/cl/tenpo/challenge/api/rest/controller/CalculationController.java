45724b9d74d7e3c54c6fc6e72564598d89247146
Jose Miguel <jmnr.1311993@gmail.com>
2023-01-31 15:53:24 -0500

    chore: added swagger

--- src/main/java/cl/tenpo/challenge/api/rest/controller/CalculationController.java
+++ src/main/java/cl/tenpo/challenge/api/rest/controller/CalculationController.java
@@ -2,6 +2,8 @@ package cl.tenpo.challenge.api.rest.controller;
 
 import cl.tenpo.challenge.api.dto.CalculationDto;
 import cl.tenpo.challenge.api.service.CalculationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "Calculations", description = "Calculation of operations between two values")
 public class CalculationController {
     private final CalculationService calculationService;
 
     @GetMapping("/calculations/percentages")
+    @ApiOperation(value = "Calculation of percentages")
     public ResponseEntity<CalculationDto> calculatePercentage(
             @RequestParam(name = "firstValue") Integer firstValue,
             @RequestParam(name = "secondValue") Integer secondValue) {
